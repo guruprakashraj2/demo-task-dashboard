607c8159cc99c8398ab7c2a27d530c4b8525b20e
Guruprakashraj <essakiprakash26@gmail.com>
2025-12-11 21:55:03 +0530

    updated overall

--- tasks/views.py
+++ tasks/views.py
@@ -1,31 +1,42 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from .models import Task
 from .serializers import TaskSerializer
 import requests
 
-# CRUD API for Tasks
+
+@method_decorator(csrf_exempt, name='dispatch')
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    # Fix CSRF issue for demo frontend
-    @csrf_exempt
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+    # FIXED: PATCH / COMPLETE BUTTON NOW WORKS
+    def partial_update(self, request, *args, **kwargs):
+        task = self.get_object()
 
-# Dashboard view
+        # Only update fields provided
+        for field, value in request.data.items():
+            setattr(task, field, value)
+
+        task.save()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 def dashboard(request):
     return render(request, 'tasks/dashboard.html')
 
-# User API integration
+
 @api_view(['GET'])
 def user_api(request):
     try:
-        response = requests.get('https://jsonplaceholder.typicode.com/users/1')
+        response = requests.get("https://jsonplaceholder.typicode.com/users/1")
         return Response(response.json())
     except Exception as e:
         return Response({"error": str(e)})
